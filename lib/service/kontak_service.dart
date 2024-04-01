88fbec50391a8ad6f76561801b820e12bed1bd7d
Bagus Satrio <bagus.satrio.ft21@mail.umy.ac.id>
2024-04-01 12:37:18 +0700

    Menambahkan metode FetchPeople untuk mengambil data dari server

--- lib/service/kontak_service.dart
+++ lib/service/kontak_service.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
+import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+
 class KontakService {
   final String baseUrl = 'https://paml.tatiumy.com/api/';
   final String endpoint = 'people';
@@ -10,20 +12,33 @@ class KontakService {
   }
 
   Future<http.Response> addPerson(Map<String, String> data, File? file) async {
-    var request = http.MultipartRequest(
-      'POST', 
-      getUri(endpoint)
-      )
-        ..fields.addAll(data)
-        ..headers['Content-Type'] = 'application/json';
+    var request = http.MultipartRequest('POST', getUri(endpoint))
+      ..fields.addAll(data)
+      ..headers['Content-Type'] = 'application/json';
 
-    if (file != null){
+    if (file != null) {
       request.files.add(await http.MultipartFile.fromPath('gambar', file.path));
-    }
-    else{
+    } else {
       request.files.add(http.MultipartFile.fromString('gambar', ''));
     }
 
     return await http.Response.fromStream(await request.send());
+  }
+
+  Future<List<dynamic>> fetchPeople() async {
+    var response = await http.get(
+        getUri(
+          endpoint,
+        ),
+        headers: {
+          "Accept": "application/json",
+        });
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> decodedResponse = json.decode(response.body);
+      return decodedResponse['people'];
+    } else {
+      throw Exception('Failed to load people: ${response.reasonPhrase}');
+    }
   }
 }
