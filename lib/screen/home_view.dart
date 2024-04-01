321fa1b08e48a34888c568fedf383767d437cf8e
Bagus Satrio <bagus.satrio.ft21@mail.umy.ac.id>
2024-04-01 13:17:03 +0700

    mengimplementasi FutureBuilder untuk menampilkan daftar orang dengan loading and error handling pada HomeView

--- lib/screen/home_view.dart
+++ lib/screen/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:praktikum5/controller/kontak_controller.dart';
+import 'package:praktikum5/model/person.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -18,7 +19,34 @@ class _HomeViewState extends State<HomeView> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Daftar kontak"),),
+      appBar: AppBar(title: Text("Daftar kontak"),),body : FutureBuilder<List<Person>>(
+          future: _controller.getPeople(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('Error: ${snapshot.error}'),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data?.length ?? 0,
+                itemBuilder: (context, index) {
+                  Person person = snapshot.data![index];
+                  return ListTile(
+                    title: Text(person.nama),
+                    subtitle: Text(person.email),
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(person.foto),
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        ),
     );
   }
 }
